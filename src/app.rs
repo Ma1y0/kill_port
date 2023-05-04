fff2e56b41a6552bfbe6675a9710d8d13941c30a
Ma1y0 <matyas.barr@gmail.com>
2023-05-04 09:40:17 +0200

    alfa version

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,8 @@
-use std::net::TcpListener;
-
 use clap::Parser;
+use std::process::Command;
+use netstat::{get_sockets_info, AddressFamilyFlags, ProtocolFlags, SocketInfo, ProtocolSocketInfo};
 
+// Arguments
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -13,9 +14,34 @@ pub fn get_port() -> u16 {
     Args::parse().port
 }
 
-pub fn is_port_in_use(port: u16) -> bool {
-    match TcpListener::bind(("0.0.0.0", port)) {
-        Ok(_) => false,
-        Err(_) => true
+fn read_local_port(socket_info: &SocketInfo) -> Option<u16> {
+    match &socket_info.protocol_socket_info {
+        ProtocolSocketInfo::Udp(_) => None,
+        ProtocolSocketInfo::Tcp(tcp_info) => Some(tcp_info.local_port)
+    }
+}
+
+pub fn get_pid(port: u16) -> Option<Vec<u32>> {
+    let sockets = get_sockets_info(AddressFamilyFlags::IPV4, ProtocolFlags::TCP).unwrap();
+    
+    for i in sockets {
+        if let Some(local_port) = read_local_port(&i) {
+            if local_port == port {
+                return Some(i.associated_pids)
+            }
+        } else {
+            return None;
+        }
+    }
+
+    None
+}
+
+pub fn kill_port(pid: u32) -> Result<(), String> {
+    let output = Command::new("kill").arg(format!("{}", pid)).output().unwrap();
+    if output.status.success() {
+        Ok(())
+    } else {
+        Err("Somethin went wrong".to_string())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
-use kill_port::app::{get_port, is_port_in_use};
+use kill_port::app::{get_port, get_pid, kill_port};
 
 fn main() {
+    // Get port from arguments
     let port = get_port();
-    println!("{}", port);
-    let in_use = is_port_in_use(port);
-    println!("{}", in_use);
+    let pid = match get_pid(port) {
+        Some(pid) => pid,
+        None => {
+            eprintln!("There is nothing running on port :{}", port);
+            return
+        }
+    };
+
+    // Get pid of the process
+    let pid = if let Some(pid) = pid.get(0) {
+        pid.to_owned()
+    } else {
+        eprintln!("Failed to get pid of process running on port :{}", port);
+        return
+    };
+
+    // Try to kill the process
+    if let Err(e) = kill_port(pid) {
+        panic!("{}", e);
+    } else {
+        println!("Successfully kill process running on port :{} with pid {}", port, pid);
+    }
 }
